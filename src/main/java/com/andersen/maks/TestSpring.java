fdadfe9ab973a94ef5f16cee2a34c24054c7ad99
zeComedian <maks.feit@gmail.com>
2019-03-21 15:01:23 +0200

    Initial commit

---
+++ src/main/java/com/andersen/maks/TestSpring.java
@@ -0,0 +1,15 @@
+package com.andersen.maks;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        testBean.printText();
+        context.close();
+    }
+}
